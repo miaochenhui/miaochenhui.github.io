a268aaf91b73677438b0eba8234d2f00c89d935a
mch <mchjava@sina.com>
2016-05-25 19:56:06 +0800

    new born

---
+++ js/myplaylist.js
@@ -0,0 +1,329 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'music/1.mp3',
+        oga:'music/1.ogg',
+        title:'像中了枪一样',
+        artist:'白智英',
+        rating:5,
+        price:'',
+        duration:'03:59',
+        cover:'music/1.png',
+		buyText:'白智英   像中了枪一样'
+    },
+	{
+        mp3:'music/2.mp3',
+        oga:'music/1.ogg',
+        title:'黑色毛衣',
+        artist:'刘瑞琦',
+        rating:2.5,
+        price:'',
+        duration:'04:19',
+        cover:'music/1.png',
+		buyText:'刘瑞琦   黑色毛衣'
+    },
+	{
+        mp3:'music/3.mp3',
+        oga:'music/1.ogg',
+        title:'黑色毛衣',
+        artist:'周杰伦',
+        rating:5,
+        price:'',
+        duration:'04:12',
+        cover:'music/1.png',
+		buyText:'周杰伦   黑色毛衣'
+    },
+	{
+        mp3:'music/4.mp3',
+        oga:'music/1.ogg',
+        title:'可爱女人',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'03:56',
+        cover:'music/1.png',
+		buyText:'周杰伦   可爱女人'
+    },
+	{
+        mp3:'music/5.mp3',
+        oga:'music/1.ogg',
+        title:'安静听完这一首',
+        artist:'伊稀',
+        rating:2.5,
+        price:'',	
+        duration:'04:41',
+        cover:'music/1.png',
+		buyText:'伊稀   安静听完这一首'
+    },
+	{
+        mp3:'music/6.mp3',
+        oga:'music/1.ogg',
+        title:'我爱你',
+        artist:'卢广仲',
+        rating:2.5,
+        price:'',
+        duration:'04:45',
+        cover:'music/1.png',
+		buyText:'卢广仲   我爱你'
+    },
+	{
+        mp3:'music/7.mp3',
+        oga:'music/1.ogg',
+        title:'七里香',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:59',
+        cover:'music/1.png',
+		buyText:'周杰伦   七里香'
+    },
+	{
+        mp3:'music/8.mp3',
+        oga:'music/1.ogg',
+        title:'安静',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'05:34',
+        cover:'music/1.png',
+		buyText:'周杰伦   安静'
+    },{
+        mp3:'music/9.mp3',
+        oga:'music/1.ogg',
+        title:'彩虹',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:32',
+        cover:'music/1.png',
+		buyText:'周杰伦   彩虹'
+    },{
+        mp3:'music/10.mp3',
+        oga:'music/1.ogg',
+        title:'明明就',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:18',
+        cover:'music/1.png',
+		buyText:'周杰伦   明明就'
+    },{
+        mp3:'music/11.mp3',
+        oga:'music/1.ogg',
+        title:'爱情悬崖',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:22',
+        cover:'music/1.png',
+		buyText:'周杰伦   爱情悬崖'
+    },{
+        mp3:'music/12.mp3',
+        oga:'music/1.ogg',
+        title:'简单爱',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:31',
+        cover:'music/1.png',
+		buyText:'周杰伦   简单爱'
+    },{
+        mp3:'music/13.mp3',
+        oga:'music/1.ogg',
+        title:'算什么男人',
+        artist:'周杰伦',
+        rating:2.5,
+        price:'',
+        duration:'04:48',
+        cover:'music/1.png',
+		buyText:'周杰伦   算什么男人'
+    },{
+        mp3:'music/14.mp3',
+        oga:'music/1.ogg',
+        title:'我以为',
+        artist:'品冠',
+        rating:2.5,
+        price:'',
+        duration:'04:58',
+        cover:'music/1.png',
+		buyText:'品冠   我以为'
+    },{
+        mp3:'music/15.mp3',
+        oga:'music/1.ogg',
+        title:'你拿什么来爱我',
+        artist:'崔天琪',
+        rating:2.5,
+        price:'',
+        duration:'03:38',
+        cover:'music/1.png',
+		buyText:'崔天琪   你拿什么来爱我'
+    },{
+        mp3:'music/16.mp3',
+        oga:'music/1.ogg',
+        title:'眼睛到地面的距离',
+        artist:'崔天琪',
+        rating:2.5,
+        price:'',
+        duration:'04:04',
+        cover:'music/1.png',
+		buyText:'崔天琪   眼睛到地面的距离'
+    },{
+        mp3:'music/17.mp3',
+        oga:'music/1.ogg',
+        title:'我是真的爱你',
+        artist:'张信哲',
+        rating:2.5,
+        price:'',
+        duration:'05:05',
+        cover:'music/1.png',
+		buyText:'张信哲   我是真的爱你'
+    },{
+        mp3:'music/18.mp3',
+        oga:'music/1.ogg',
+        title:'我最亲爱的',
+        artist:'张惠妹',
+        rating:2.5,
+        price:'',
+        duration:'04:44',
+        cover:'music/1.png',
+		buyText:'张惠妹   我最亲爱的'
+    },{
+        mp3:'music/19.mp3',
+        oga:'music/1.ogg',
+        title:'演员',
+        artist:'徐薇',
+        rating:2.5,
+        price:'',
+        duration:'04:21',
+        cover:'music/1.png',
+		buyText:'徐薇   演员'
+    },{
+        mp3:'music/20.mp3',
+        oga:'music/1.ogg',
+        title:'平凡之路',
+        artist:'朴树',
+        rating:2.5,
+        price:'',
+        duration:'05:01',
+        cover:'music/1.png',
+		buyText:'朴树   平凡之路'
+    },{
+        mp3:'music/21.mp3',
+        oga:'music/1.ogg',
+        title:'不将就',
+        artist:'李荣浩',
+        rating:2.5,
+        price:'',
+        duration:'05:13',
+        cover:'music/1.png',
+		buyText:'李荣浩   不将就'
+    },{
+        mp3:'music/22.mp3',
+        oga:'music/1.ogg',
+        title:'李白',
+        artist:'李荣浩',
+        rating:2.5,
+        price:'',
+        duration:'04:33',
+        cover:'music/1.png',
+		buyText:'李荣浩   李白'
+    },{
+        mp3:'music/23.mp3',
+        oga:'music/1.ogg',
+        title:'模特',
+        artist:'李荣浩',
+        rating:2.5,
+        price:'',
+        duration:'05:06',
+        cover:'music/1.png',
+		buyText:'李荣浩   模特'
+    },{
+        mp3:'music/24.mp3',
+        oga:'music/1.ogg',
+        title:'她说',
+        artist:'林俊杰',
+        rating:2.5,
+        price:'',
+        duration:'05:20',
+        cover:'music/1.png',
+		buyText:'林俊杰   她说'
+    },{
+        mp3:'music/25.mp3',
+        oga:'music/1.ogg',
+        title:'当你',
+        artist:'林俊杰',
+        rating:2.5,
+        price:'',
+        duration:'04:11',
+        cover:'music/1.png',
+		buyText:'林俊杰   当你'
+    },{
+        mp3:'music/26.mp3',
+        oga:'music/1.ogg',
+        title:'第几个100天',
+        artist:'林俊杰',
+        rating:2.5,
+        price:'',
+        duration:'04:40',
+        cover:'music/1.png',
+		buyText:'林俊杰   第几个100天'
+    },{
+        mp3:'music/27.mp3',
+        oga:'music/1.ogg',
+        title:'Good爱',
+        artist:'欧汉声',
+        rating:2.5,
+        price:'',
+        duration:'03:41',
+        cover:'music/1.png',
+		buyText:'欧汉声   Good爱'
+    },{
+        mp3:'music/28.mp3',
+        oga:'music/1.ogg',
+        title:'一生有你',
+        artist:'水木年华',
+        rating:2.5,
+        price:'',
+        duration:'04:18',
+        cover:'music/1.png',
+		buyText:'水木年华   一生有你'
+    },{
+        mp3:'music/29.mp3',
+        oga:'music/1.ogg',
+        title:'无与伦比的美丽',
+        artist:'苏打绿',
+        rating:2.5,
+        price:'',
+        duration:'05:06',
+        cover:'music/1.png',
+		buyText:'苏打绿   无与伦比的美丽'
+    },{
+        mp3:'music/30.mp3',
+        oga:'music/1.ogg',
+        title:'演员',
+        artist:'薛之谦',
+        rating:2.5,
+        price:'',
+        duration:'04:21',
+        cover:'music/1.png',
+		buyText:'薛之谦   演员'
+    },{
+        mp3:'music/31.mp3',
+        oga:'music/1.ogg',
+        title:'上心',
+        artist:'郑欣宜',
+        rating:2.5,
+        price:'',
+        duration:'03:49',
+        cover:'music/1.png',
+		buyText:'郑欣宜   上心'
+    },
+];
